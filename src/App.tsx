f1ddfe139bb9b6506cfc08de55e3a87472eaf426
Muhaymin Bin Mehmood <72308336+muhaymin-bin-mehmood@users.noreply.github.com>
2022-11-16 03:48:57 +0500

    Multistep Form Custom Hook With React And TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { FormEvent, useState } from 'react'
 import { AccountForm } from './AccountForm';
 import { AddressForm } from './AddressForm';
@@ -51,36 +52,45 @@ function App() {
   }
 
   return (
-    <div
-      style={{
-        position: "relative",
-        background: "white",
-        border: "1px solid black",
-        padding: "2rem",
-        margin: "1rem",
-        borderRadius: ".5rem",
-        fontFamily: "Arial",
-        maxWidth: 'max-content'
-      }}
-    >
-      <form onSubmit={onSubmit}>
-        <div style={{ position: 'absolute', top: '.5rem', right: '.5rem' }}>
-          {currentStepIndex + 1} / {steps.length}
-        </div>
-        {step}
-        <div
-          style={{
-            marginTop: "1rem",
-            display: "flex",
-            gap: ".5rem",
-            justifyContent: "flex-end",
-          }}
-        >
-          {!isFirstStep ? <button type="button" onClick={back}>Back</button> : ''}
-          <button type="submit">{isLastStep ? "Finish" : "Next"}</button>
-        </div>
-      </form>
-    </div>
+    <>
+      <div
+        style={{
+          position: "relative",
+          background: "white",
+          border: "1px solid black",
+          padding: "2rem",
+          margin: "1rem",
+          borderRadius: ".5rem",
+          fontFamily: "Arial",
+          maxWidth: 'max-content'
+        }}
+      >
+        <form onSubmit={onSubmit}>
+          <div style={{ position: 'absolute', top: '.5rem', right: '.5rem' }}>
+            {currentStepIndex + 1} / {steps.length}
+          </div>
+          {step}
+          <div
+            style={{
+              marginTop: "1rem",
+              display: "flex",
+              gap: ".5rem",
+              justifyContent: "flex-end",
+            }}
+          >
+            {!isFirstStep ? <button type="button" onClick={back}>Back</button> : ''}
+            <button type="submit">{isLastStep ? "Finish" : "Next"}</button>
+          </div>
+        </form>
+      </div>
+
+
+      <ul style={{ listStyle: 'none', marginTop: '40px' }}>
+        {Object.entries(data).map((item, index) => (
+          <li key={index} style={{ fontWeight: 600, marginBottom: '10px' }}>{item[0].toUpperCase()}: {item[1]}</li>
+        ))}
+      </ul>
+    </>
   )
 }
 
