6ac7e7e813a634af0a27a005a1c74c5e6b40fa20
Muhaymin Bin Mehmood <72308336+muhaymin-bin-mehmood@users.noreply.github.com>
2022-11-16 03:35:03 +0500

    Multistep Form Custom Hook With React And TypeScript

---
+++ src/useMultiStepForm.tsx
@@ -0,0 +1,36 @@
+import { ReactElement, useState } from "react";
+
+
+// This is a custom hook which controls all the Functionality of Basic Steps like next, previous, go to specific steps, current step, last step, etc
+export function useMultiStepForm(steps: ReactElement[]) {
+    const [currentStepIndex, setCurrentStepIndex] = useState(0)
+
+    function next() {
+        setCurrentStepIndex(i => {
+            if (i >= steps.length - 1) return i
+            return i + 1
+        })
+    }
+
+    function back() {
+        setCurrentStepIndex(i => {
+            if (i <= 0) return i
+            return i - 1
+        })
+    }
+
+    function goTo(index: number) {
+        setCurrentStepIndex(index)
+    }
+
+    return {
+        currentStepIndex,
+        step: steps[currentStepIndex],
+        steps,
+        isFirstStep: currentStepIndex === 0,
+        isLastStep: currentStepIndex === steps.length - 1,
+        goTo,
+        next,
+        back,
+    }
+}
